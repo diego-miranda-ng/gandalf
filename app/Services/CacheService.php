bf1a8312c4b9b75b9f9f7e105437b674eb80b848
Diego Miranda Neves Gomes <diego.miranda.ng@gmail.com>
2025-01-29 03:43:20 +0000

    Manually defining each folders are allowed in the context

--- app/Services/CacheService.php
+++ app/Services/CacheService.php
@@ -7,6 +7,21 @@ class CacheService
     private const OWNER = 'diego-miranda-ng';
     private const REPO = 'gandalf';
 
+    private const ALLOWED_PATHS = [
+        'app/Services',
+        'app/Models',
+        'app/Providers',
+        'app/Http/Controllers',
+        'app/Http/Middleware',
+        'app/Http/Requests',
+        'app/Http/Resources',
+        'app/Http/Controllers',
+        'routes',
+        'bootstrap',
+        'public',
+        'config',
+    ];
+
     public function __construct(private GitHubService $gitHubService, private GeminiService $geminiService)
     {}
 
@@ -14,7 +29,7 @@ class CacheService
     {
         $cachedContent = $this->geminiService->getCachedContent();
         if (empty($cachedContent['cachedContents'])) {
-            $files = $this->gitHubService->getRepositoryContent(self::OWNER, self::REPO);
+            $files = $this->getAllFiles();
 
             $content = '';
             foreach ($files as $file) {
@@ -27,5 +42,16 @@ class CacheService
         }
         
         return $cachedContent['cachedContents'][0]['name'];
+    }
+
+    private function getAllFiles(): array
+    {
+        $files = [];
+        collect(self::ALLOWED_PATHS)->each(function ($path) use (&$files) {
+            $downloadedFiles = $this->gitHubService->getRepositoryContent(self::OWNER, self::REPO, $path)?->toArray() ?: [];
+            $files = array_merge($files, $downloadedFiles);
+        });
+
+        return $files;
     }
 }
